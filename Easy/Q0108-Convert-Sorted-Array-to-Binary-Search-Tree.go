335ee909a4bea9d5b947850b0ec0412990ca881e
mujapes <222509507+mujapes@users.noreply.github.com>
2025-09-23 01:09:10 +0100

    Create Q0108-Convert-Sorted-Array-to-Binary-Search-Tree.go

---
+++ Easy/Q0108-Convert-Sorted-Array-to-Binary-Search-Tree.go
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func sortedArrayToBST(nums []int) *TreeNode {
+    if len(nums) > 0 {
+        return &TreeNode{Val: nums[len(nums)/2], Left: sortedArrayToBST(nums[:len(nums)/2]), Right: sortedArrayToBST(nums[(len(nums)/2)+1:])}
+    } else {return nil}
+}
+
+// Runtime: 0 ms, Beats 100.00%
+// Memory: 5.38 MB, Beats 28.19%
