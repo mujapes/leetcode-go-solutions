5eee9d87eb1cf7778d772a8c8483e0918bdb68a4
mujapes <222509507+mujapes@users.noreply.github.com>
2025-09-24 01:57:52 +0100

    Create Q0110-Balanced-Binary-Tree.go

---
+++ Easy/Q0110-Balanced-Binary-Tree.go
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func depthIfSimilar(node *TreeNode, depth int) int {
+    if node == nil {return depth}
+    depth++
+    leftDepth := depthIfSimilar(node.Left, depth)
+    rightDepth := depthIfSimilar(node.Right, depth)
+    if leftDepth == -1 || rightDepth == -1 || leftDepth-rightDepth > 1 || rightDepth-leftDepth > 1 {return -1}
+    return max(leftDepth, rightDepth)
+}
+
+func isBalanced(root *TreeNode) bool {
+    if depthIfSimilar(root, 0) == -1 {return false}
+    return true
+}
+
+// Runtime: 0 ms, Beats 100.00%
+// Memory: 7.36 MB, Beats 56.57%
