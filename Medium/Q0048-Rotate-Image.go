4014f9f02ff2f880be1a7d78c8e7ac0777f27acd
mujapes <222509507+mujapes@users.noreply.github.com>
2025-10-10 18:25:36 +0100

    Update Q0048-Rotate-Image.go

--- Medium/Q0048-Rotate-Image.go
+++ Medium/Q0048-Rotate-Image.go
@@ -1,22 +1,14 @@
 func rotate(matrix [][]int)  {
-    topLeft, topRight, botRight, botLeft := []int{0, 0}, []int{0, len(matrix)-1}, []int{len(matrix)-1, len(matrix)-1}, []int{len(matrix)-1, 0}
-    for topLeft[1] < topRight[1] {
-        // 4 pointers have values swapped (clockwise) then move clockwise.
-        // Once topLeft pointer reaches original topRight position break 
-        // and set pointers to corners of inner matrix
-        for topLeft[1] < topRight[1] {
-            matrix[topLeft[0]][topLeft[1]], matrix[topRight[0]][topRight[1]],  matrix[botRight[0]][botRight[1]], matrix[botLeft[0]][botLeft[1]] = matrix[botLeft[0]][botLeft[1]], matrix[topLeft[0]][topLeft[1]], matrix[topRight[0]][topRight[1]],  matrix[botRight[0]][botRight[1]]
-            
-            topLeft[1]++
-            topRight[0]++
-            botRight[1]--
-            botLeft[0]--
+    n := len(matrix)-1
+    for layer := range (n+1)/2 {
+        // 4 elements 90x degrees apart swapped clockwise, next 4 elements are
+        // nearest clockwise neighbours, indent layer + reset rotation at >= 90 degrees
+        for rot := layer; rot < n-layer; rot++ {
+            matrix[layer][rot], matrix[rot][n-layer], matrix[n-layer][n-rot], matrix[n-rot][layer] = 
+                matrix[n-rot][layer], matrix[layer][rot], matrix[rot][n-layer], matrix[n-layer][n-rot]
         }
-        topLeft[0], topLeft[1], topRight[0], topRight[1], botRight[0], botRight[1], botLeft[0], botLeft[1] = botLeft[0]+1, botLeft[1]+1, topLeft[0]+1, topLeft[1]-1, topRight[0]-1, topRight[1]-1, botRight[0]-1, botRight[1]+1
-        // If inner matrix 1x1 or non-existant topLeft and topRight pointers 
-        // will be in the same spot or at previous botRight and botLeft positions
     }
 }
 
 // Runtime: 0 ms, Beats 100.00%
-// Memory: 4.12 MB, Beats 60.41%
+// Memory: 4.09 MB, Beats 97.35%
